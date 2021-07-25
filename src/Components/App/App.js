136960142fc28305cd19627f5ad95fb3e7c4dc52
Busra Demirci <bdemirc1@binghamton.edu>
2021-07-25 16:33:26 +0300

    Passing props

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,18 +1,49 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { Playlist } from '../Playlist/Playlist.js'
 import { SearchBar } from '../SearchBar/SearchBar.js'
 import { SearchResults } from '../SearchResults/SearchResults.js'
+import React from 'react';
 
-function App() {
-  return (
+export class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { searchResults: [], 
+                  playlistName: "Sample", 
+                  playlistTracks: [{name: "Song1", artist: "Artist1", album: "Album1", id: 1},
+                                   {name: "Song2", artist: "Artist2", album: "Album2", id: 2}]};
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+  }
+  
+  addTrack(track){
+    if(this.state.playlistTracks.find(saved => saved.id === track.id)){
+      return;
+    }
+    this.setState({ playlistTracks : this.state.playlistTracks.push(track) });
+  }
+
+  removeTrack(track){
+    const filtered = this.state.playlistTracks.filter(saved => saved.id === track.id);
+    this.state({ playlistTracks : filtered});
+  }
+
+  updatePlaylistName(newName){
+    this.setState({ playlistName: newName});
+  }
+  
+  render(){
+    return (
     <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
       <div className="App">
+        <SearchBar/>
         <div className="App-playlist"></div>
+        <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
+        <Playlist playlistName = { this.state.playlistName} playlistTracks = {this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName}/>
       </div>
     </div>
-  );
+    );
+  }
 }
-
-export default App;
